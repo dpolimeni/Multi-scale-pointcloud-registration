810d781649b5ec0be7b545ed819c58d74a9a59f9
dpolimeni <diego.polimeni99@gmail.com>
2024-07-14 17:48:57 +0200

    add transform method

---
+++ src/or_pcd/Preprocessor/Downsamplers/__init__.py
@@ -0,0 +1,4 @@
+from or_pcd.Preprocessor.Downsamplers.farthestDownsampler import FarthestDownsampler
+from or_pcd.Preprocessor.Downsamplers.randomDownsampler import RandomDownsampler
+from or_pcd.Preprocessor.Downsamplers.voxelDownsampler import VoxelDownsampler
+
